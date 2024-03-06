862c3b31bda5c03121c66c8537ec146ce2d5f0cf
rudyorre <rudyorre@gmail.com>
2024-03-06 15:02:27 -0800

    feat: Add tailwindcss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import '@/style.css'
+import '@/index.css'
 import App from '@/App.vue'
 
 import HomeView from '@/views/HomeView.vue'
