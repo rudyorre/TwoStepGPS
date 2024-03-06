76523545a3338ec60bc353e075c096e6382f886f
rudyorre <rudyorre@gmail.com>
2024-03-06 10:57:55 -0800

    refactor: Add vue-router functionality

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="dashboard">
-      <h1>This is a dashboard page</h1>
-    </div>
-  </template>
+  <div class="dashboard">
+    <h1 class="text-red-500">This is a dashboard page</h1>
+  </div>
+</template>
   
