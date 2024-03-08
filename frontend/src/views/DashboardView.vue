3a3ccf6e6dc86a39701959106bec77bf9444e957
rudyorre <rudyorre@gmail.com>
2024-03-08 10:42:52 -0800

    fix: Reload handler for router

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 // import Navbar from '@/components/Navbar.vue'
 import Sidebar from '@/components/Sidebar.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
+const redirectReload = async () => {
+  await router.push({ path: '/dashboard' })
+  router.go(0)
+}
 </script>
 
 <template>
