27786718d335011459c53c91296456da4da22eeb
rudyorre <rudyorre@gmail.com>
2024-03-09 17:24:01 -0800

    fix: Remove unused reloadRouter

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
 // import Navbar from '@/components/Navbar.vue'
 import Sidebar from '@/components/Sidebar.vue'
-import { useRouter } from 'vue-router'
-const router = useRouter()
-
-const redirectReload = async () => {
-  await router.push({ path: '/dashboard' })
-  router.go(0)
-}
 </script>
 
 <template>
